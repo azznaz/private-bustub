5ee5a86073bb9df6b70d84f005d07a18e858e332
azznaz <1751672662@qq.com>
2021-09-20 18:27:40 +0800

    finish pro1

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -19,30 +19,28 @@ LRUReplacer::LRUReplacer(size_t num_pages) { max_num_pages_ = num_pages; }
 LRUReplacer::~LRUReplacer() = default;
 
 bool LRUReplacer::Victim(frame_id_t *frame_id) {
-  //lru_latch_.lock();
   if (lru_.empty()) {
-    //lru_latch_.unlock();
+
     return false;
   }
   *frame_id = lru_.front();
   lru_.pop_front();
-  //lru_latch_.unlock();
   return true;
 }
 
 void LRUReplacer::Pin(frame_id_t frame_id) {
-  //lru_latch_.lock();
+
   for (auto iter = lru_.begin(); iter != lru_.end(); iter++) {
     if ((*iter) == frame_id) {
       lru_.erase(iter);
       break;
     }
   }
-  //lru_latch_.unlock();
+
 }
 
 void LRUReplacer::Unpin(frame_id_t frame_id) {
-  //lru_latch_.lock();
+
   bool flag = false;
   for (auto ele : lru_) {
     if (ele == frame_id) {
@@ -53,7 +51,6 @@ void LRUReplacer::Unpin(frame_id_t frame_id) {
   if (!flag) {
     lru_.push_back(frame_id);
   }
-  //lru_latch_.unlock();
 }
 
 size_t LRUReplacer::Size() { return lru_.size(); }
